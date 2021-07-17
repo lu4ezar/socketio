6c2a5262c55a22bae5462965ef3607484dc1cb29
lu4ezar <misha-shisha@mail.ru>
2021-07-17 17:44:25 +0700

    cleanup

--- backend/src/io.js
+++ backend/src/io.js
@@ -1,20 +1,3 @@
-/*const sio = require('socket.io');
-
-let io = null;
-
-exports.initialize = server => {
-	io = sio(server);
-	io.on('connection', (socket) => {
-		console.log(Object.keys(socket));
-		console.debug(`A user connected with ${socket.id}`);
-		socket.on('disconnect', () => {
-			console.debug(`A user disconnected with ${socket.id}`);
-		});
-	});
-};
-
-exports.io = () => io;*/
-
 const IO = require('koa-socket-2');
 
 let io = null;
@@ -23,17 +6,11 @@ exports.initialize = (server) => {
   io = new IO();
   io.attach(server);
   io.on('connection', (ctx, data) => {
-    console.log(Object.keys(ctx));
-    console.log(data);
-    // console.debug(`A user connected with ${socket.id}`);
-    console.debug(`A user connected`);
+    console.debug(`A user connected with ${ctx.id}`);
     io.on('disconnect', () => {
-      console.debug(`A user disconnected`);
-      // console.debug(`A user disconnected with ${socket.id}`);
+      console.debug(`A user disconnected with ${ctx.id}`);
     });
   });
 };
 
 exports.io = () => io;
-
-// "start": "concurrently \"npm run backend\" \"npm run frontend\"",
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,44 +1,3 @@
-/*
-const dotenv = require('dotenv').config({
-	path: 'variables.env'
-});
-
-const Koa = require('koa');
-const app = new Koa();
-
-const logger = require('koa-logger');
-if (process.env.NODE_ENV !== 'test') {
-	app.use(logger());
-}
-
-const bodyParser = require('koa-body');
-app.use(bodyParser());
-
-const path = require('path');
-const serve = require('koa-static');
-app.use(serve(path.join(__dirname, '../public')));
-
-const port = process.env.PORT || 3001;
-
-const server = app.listen(port, () => {
-	console.log(`I am server and my port is ${port}`);
-});
-
-require('./io').initialize(server);
-const global_socket = require('./io').io();
-
-const heartbeat = () => {
-	const pulse = Math.ceil(Math.random() * (160 - 60) + 60);
-	return pulse;
-};
-
-setInterval(() => {
-	global_socket.emit('PULSE', heartbeat());
-}, 3000);
-
-module.exports = app;
-*/
-
 const Koa = require('koa');
 const app = new Koa();
 const logger = require('koa-logger');
@@ -47,26 +6,21 @@ app.use(logger());
 const bodyParser = require('koa-body');
 app.use(bodyParser());
 
-app.use(async ctx => {
-	// console.log(Object.keys(ctx));
-	ctx.body = 'hey you'
+app.use(async (ctx) => {
+  ctx.body = 'hey you';
 });
 
 require('./io').initialize(app);
 const global_socket = require('./io').io();
-console.log(Object.keys(global_socket));
+
 app.listen(3001, () => {
-	console.log('server has started...');
+  console.log('server has started');
 });
 
-const heartbeat = () => {
-	const pulse = Math.ceil(Math.random() * (160 - 60) + 60);
-	return pulse;
-};
+const heartbeat = () => Math.ceil(Math.random() * (160 - 60) + 60);
 
 setInterval(() => {
-	// console.log(Object.keys(io));
-	global_socket.socket.emit('PULSE', heartbeat());
-}, 3000);
+  global_socket.socket.emit('PULSE', heartbeat());
+}, 1000);
 
 module.exports = app;
