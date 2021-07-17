ade50e7ce09b9e4c008545868160fab36bf03e70
lu4ezar <misha-shisha@mail.ru>
2021-07-17 17:43:57 +0700

    switch to react-sparklines

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import Sparkline from '@rowno/sparkline';
 import { Card, Container } from '@material-ui/core';
+import { Sparklines, SparklinesBars } from 'react-sparklines';
 
 import socketIOClient from 'socket.io-client';
 const socket = socketIOClient('http://localhost:3001');
@@ -8,32 +8,24 @@ const socket = socketIOClient('http://localhost:3001');
 const initialValues = [789, 880, 676, 200, 890, 677, 900];
 
 const App = () => {
-	const [values, setValues] = React.useState(initialValues);
-	React.useEffect(() => {
-		const handler = data => setValues([...values.slice(1), data]);
-		socket.on('PULSE', handler);
-		return () => {
-			socket.off('PULSE', handler);
-		};
-	});
+  const [values, setValues] = React.useState(initialValues);
+  React.useEffect(() => {
+    const handler = (data) => setValues([...values.slice(1), data]);
+    socket.on('PULSE', handler);
+    return () => {
+      socket.off('PULSE', handler);
+    };
+  });
 
-	const lines = [
-		{
-			values,
-			colors: {
-				area: 'rgba(217, 227, 237, 0.5)',
-				line: '#193652'
-			}
-		}
-	];
-
-	return (
-		<Container>
-			<Card>
-				<Sparkline width={560} height={120} lines={lines} />
-			</Card>
-		</Container>
-	);
+  return (
+    <Container>
+      <Card>
+        <Sparklines data={values}>
+          <SparklinesBars />
+        </Sparklines>
+      </Card>
+    </Container>
+  );
 };
 
 export default App;
